7ac8f06c6e60363c5c3fc1df07510a9cb8a329f2
James Benner <hello@jamesbenner.com>
2025-10-24 17:57:51 -0400

    Revert "Adjust PNG processing"
    This reverts commit 76a9f30ac6e2e69f489ec81d7021403164c1eb68.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,8 @@
-import os from 'node:os';
-
-const IS_MAC = os.platform() === 'darwin';
-
 /**
  * @type {import('lint-staged').Configuration}
  */
 export default {
   '*.{js,ts,json,md}': 'eslint --fix',
-  '*.png': files => IS_MAC
-    ? files.map(file => [`pngquant --force --output ${file} ${file}`, `imageoptim ${file}`])
-    : files.map(file => `pngquant --force --output ${file} --skip-if-larger ${file}`),
+  '*.png': files => files.map(file => `pngquant --force --output ${file} --skip-if-larger ${file}`),
   'package.json': 'sort-package-json'
 };
